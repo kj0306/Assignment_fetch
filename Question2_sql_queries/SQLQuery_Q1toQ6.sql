c5d8419c2628ffddd73bb5c498d64254ee246a8f
kj0306 <72967236+kj0306@users.noreply.github.com>
2024-10-16 00:44:25 -0500

    Add files via upload

---
+++ Question2_sql_queries/SQLQuery_Q1toQ6.sql
@@ -0,0 +1,88 @@
+
+/*
+QUERY 1: What are the top 5 brands by receipts scanned for most recent month?
+*/
+
+WITH recent AS (
+    SELECT TOP 1 
+        YEAR(r.scanned_date) AS year, MONTH(r.scanned_date) AS month
+    FROM receipts_main r
+    ORDER BY r.scanned_date DESC
+),
+res AS (
+    SELECT TOP 5 i.barcode
+    FROM receipts_main r
+    JOIN receipt_items i ON r.receipt_id = i.receipt_id
+    WHERE YEAR(r.scanned_date) = (SELECT year FROM recent) 
+      AND MONTH(r.scanned_date) = (SELECT month FROM recent)
+    GROUP BY i.barcode
+    ORDER BY COUNT(i.barcode) DESC
+)
+SELECT b.brand_id, b.brand_name FROM brands_data b
+JOIN res ON b.barcode = res.barcode;
+
+-------------------------------------------------------------------------------------------------------------------
+
+/*
+Query 3: When considering average spend from receipts with 'rewardsReceiptStatus’ of ‘Accepted’ or ‘Rejected’, which is greater?
+*/
+SELECT rewards_receipt_status AS STATUS, 
+    SUM(total_spent) AS TotalSpent,
+    (SUM(total_spent) / COUNT(rewards_receipt_status)) AS AvgSpent 
+FROM receipts_main 
+GROUP BY rewards_receipt_status 
+ORDER BY AvgSpent DESC;
+
+---------------------------------------------------------------------------------------------------------------------
+
+/* 
+Query 4: When considering total number of items purchased from receipts with 'rewardsReceiptStatus’ of ‘Accepted’ or ‘Rejected’, which is greater?
+*/
+
+SELECT rewards_receipt_status AS STATUS, 
+	SUM(purchased_item_count) AS ItemsCount 
+FROM receipts_main 
+GROUP BY rewards_receipt_status 
+ORDER BY ItemsCount DESC;
+
+-----------------------------------------------------------------------------------------------------------------------
+
+/* 
+Query 5: Which brand has the most spend among users who were created within the past 6 months?
+*/
+
+WITH recent AS (
+    SELECT user_id AS users FROM users_data 
+    WHERE created_date > DATEADD(MONTH, -6, (SELECT TOP 1 created_date FROM users_data 
+    ORDER BY created_date DESC))
+),
+barcode_spending AS (
+    SELECT i.barcode, SUM(r.total_spent) AS TotalSpent FROM receipts_main r
+      JOIN receipt_items i ON r.receipt_id = i.receipt_id
+    WHERE r.user_id IN (SELECT users FROM recent)
+    GROUP BY i.barcode 
+)
+SELECT bs.barcode, bs.TotalSpent, b.brand_name FROM barcode_spending bs
+LEFT JOIN brands_data b ON bs.barcode = b.barcode
+ORDER BY bs.TotalSpent DESC;
+
+---------------------------------------------------------------------------------------------------------------------
+
+/*
+Query 6: Which brand has the most transactions among users who were created within the past 6 months?
+*/
+
+WITH recent AS (
+    SELECT user_id AS users FROM users_data 
+    WHERE created_date > DATEADD(MONTH, -6, (SELECT TOP 1 created_date FROM users_data 
+  ORDER BY created_date DESC))
+)
+SELECT i.barcode, COUNT(i.barcode) AS Transactions, b.brand_name FROM receipts_main r
+JOIN receipt_items i ON r.receipt_id = i.receipt_id 
+LEFT JOIN brands_data b ON i.barcode = b.barcode
+WHERE r.user_id IN (SELECT users FROM recent)
+GROUP BY i.barcode, b.brand_name 
+ORDER BY Transactions DESC;
+
+---------------------------------------------------------------------------------------------------------------------------
+
---
+++ Question2_sql_queries/SQLQuery_createdb.sql
@@ -0,0 +1,104 @@
+USE fetchdata;
+
+CREATE TABLE users_data(
+	user_id varchar(50) PRIMARY KEY,
+	active bit NULL,
+	created_date datetime NULL,
+	last_login_date datetime NULL,
+	user_role varchar(20) NULL,
+	sign_up_source varchar(20) NULL,
+	user_state char(2) NULL
+);
+
+BULK INSERT users_data
+FROM 'C:\kathy\users_formatted.csv'
+WITH (
+    FIELDTERMINATOR = ',',  
+    ROWTERMINATOR = '\n',
+    FIRSTROW = 2
+);
+
+SELECT * FROM users_data;
+
+CREATE TABLE receipts_main (
+    receipt_id VARCHAR(50) PRIMARY KEY,                 
+    bonus_points_earned DECIMAL NULL,
+    bonus_points_earned_reason NVARCHAR(255) NULL,
+    created_date datetime NULL,                
+    scanned_date datetime NULL,                
+    finished_date datetime NULL,          
+    modify_date datetime NULL,            
+    points_awarded_date datetime NULL,    
+    points_earned FLOAT NULL,
+    purchase_date datetime NULL,          
+    purchased_item_count INT NULL,
+    rewards_receipt_status VARCHAR(50) NOT NULL,
+    total_spent DECIMAL(10, 2) NULL,
+    user_id VARCHAR(50) NOT NULL,
+	FOREIGN KEY (user_id) REFERENCES users_data(user_id)
+);
+
+BULK INSERT receipts_main
+FROM 'C:\kathy\receipts_formatted.csv'
+WITH (
+    FIELDTERMINATOR = ',',  
+    ROWTERMINATOR = '\n',
+    FIRSTROW = 2
+);
+
+SELECT * FROM receipts_main;
+
+SELECT * FROM brands_data;
+CREATE TABLE brands_data(
+	brand_id varchar(50) NOT NULL,
+	barcode varchar(50) PRIMARY KEY,
+	category nvarchar(100) NULL,
+	category_code nvarchar(100) NULL,
+	cpg_id nvarchar(50) NOT NULL,
+	cpg_ref varchar(50) NOT NULL,
+	brand_name nvarchar(255) NOT NULL,
+	top_brand bit NOT NULL,
+	brand_code nvarchar(100) NULL
+);
+
+BULK INSERT brands_data
+FROM 'C:\kathy\brand_formatted_numeric_csv1.csv'
+WITH (
+    FIELDTERMINATOR = ',',  
+    ROWTERMINATOR = '\n',
+    FIRSTROW = 2
+);
+
+CREATE TABLE receipt_items (
+    barcode VARCHAR(50),
+    item_description VARCHAR(255) NULL,
+    finalPrice DECIMAL(10, 2) NULL,
+    itemPrice DECIMAL(10, 2) NULL,
+    needs_fetch_review BIT,
+    partner_item_id INT,  
+    prevent_target_gap_points BIT,
+    quantity_purchased INT,
+    user_flagged_barcode BIGINT NULL, 
+    user_flagged_new_item BIT,
+    user_flagged_price DECIMAL(10, 2) NULL,
+    user_flagged_quantity INT NULL,
+	receipt_id VARCHAR(50),
+    FOREIGN KEY (receipt_id) REFERENCES receipts_main(receipt_id),
+	FOREIGN KEY (barcode) REFERENCES brands_data(barcode)
+);
+
+BULK INSERT receipt_items
+FROM 'C:\kathy\receipt_items_formatted.csv'
+WITH (
+    FIELDTERMINATOR = ',',  
+    ROWTERMINATOR = '\n',
+    FIRSTROW = 2
+);
+
+SELECT * FROM receipt_items; 
+
+ALTER TABLE receipt_items
+ADD receipt_item_id INT IDENTITY(1,1);
+
+ALTER TABLE receipt_items
+ADD CONSTRAINT PK_receipt_item_id PRIMARY KEY (receipt_item_id);
